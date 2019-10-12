9c3eb55a586b1d71ebbba3a4dd843821a2fd1d37
jakazzy <jakazzy@gmail.com>
2019-10-12 12:46:13 +0000

    fix cors error

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,37 @@
 const express = require("express");
 const app = express();
-app.use((req, res) => {
-  res.json({ message: "your request was successful" });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "aufuah89ya8y",
+      title: "this is a thing",
+      description: "this is a beautiful thing",
+      imageUrl: "",
+      price: 400,
+      userId: "akhfuiaon8y"
+    },
+    {
+      _id: "ajboa89pnu9[0s",
+      title: "this is a thing",
+      description: "this is a beautiful thing",
+      imageUrl: "",
+      price: 200,
+      userId: "akhfuiaon8y"
+    }
+  ];
+  res.status(200).json(stuff);
 });
 module.exports = app;
