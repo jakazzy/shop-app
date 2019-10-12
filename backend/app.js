9ebf9144f75cde5467c11dc022772dbfa7ecb57a
jakazzy <jakazzy@gmail.com>
2019-10-12 15:58:47 +0000

    set up mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
+// MONGODB PWD: stbf6CKIQoicpx7r
+// Connection string: mongodb+srv://jida:<password>@cluster0-d9wpy.mongodb.net/test?retryWrites=true&w=majority
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://jida:stbf6CKIQoicpx7r@cluster0-d9wpy.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connection with mongoDB successful"))
+  .catch(err => {
+    console.log("unable to connect to mongodb atlas");
+    console.error(err);
+  });
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,13 +27,22 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use(bodyParser.json());
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Thing created successfully!"
+  });
+});
 app.use("/api/stuff", (req, res, next) => {
   const stuff = [
     {
       _id: "aufuah89ya8y",
       title: "this is a thing",
       description: "this is a beautiful thing",
-      imageUrl: "",
+      imageUrl:
+        "https://images.unsplash.com/photo-1562887009-7924341c5cbc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
       price: 400,
       userId: "akhfuiaon8y"
     },
@@ -27,11 +50,13 @@ app.use("/api/stuff", (req, res, next) => {
       _id: "ajboa89pnu9[0s",
       title: "this is a thing",
       description: "this is a beautiful thing",
-      imageUrl: "",
+      imageUrl:
+        "https://images.unsplash.com/photo-1562887009-7924341c5cbc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
       price: 200,
       userId: "akhfuiaon8y"
     }
   ];
   res.status(200).json(stuff);
+  next();
 });
 module.exports = app;
